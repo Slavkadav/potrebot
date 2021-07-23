771d3137b81c83f5b88721f7c6f9a7a56d898994
VDavydov <slavkadav@gmail.com>
2021-07-23 16:58:21 +0300

    move to spring web mvc

---
+++ src/main/java/ru/abe/slaves/potrebot/EventController.java
@@ -0,0 +1,52 @@
+package ru.abe.slaves.potrebot;
+
+import com.google.gson.JsonObject;
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.GroupActor;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@Slf4j
+@RestController
+@RequestMapping
+public class EventController {
+    public static final int CHAT_PEER_PREFIX = 2000000000;
+    public static final String MESSAGE_TYPE = "message_new";
+    public static final int GROUP_ID = 204764107;
+    private static final String BLANK = "https://oauth.vk.com/blank.html";
+    private static final String KEY = "e77dace000404e3754d4f0d84a651d12b52a37cb34e0ea48b7112b84d27f4190d97d15d75407ab31a2e51";
+    private final Random random = new Random();
+
+    @PostMapping
+    public String handleEvent(JsonObject eventJson) {
+        TransportClient transportClient = HttpTransportClient.getInstance();
+        VkApiClient vk = new VkApiClient(transportClient);
+        GroupActor groupActor = new GroupActor(GROUP_ID, KEY);
+        if (MESSAGE_TYPE.equals(eventJson.get("type").getAsString())) {
+            int chatId = eventJson.getAsJsonObject("object")
+                    .getAsJsonObject("message")
+                    .get("peer_id")
+                    .getAsInt();
+            chatId = chatId - CHAT_PEER_PREFIX;
+            try {
+                vk.messages().send(groupActor)
+                        .chatId(chatId)
+                        .randomId(random.nextInt())
+                        .message("ДА РАБОТАЮ Я БЛЯТЬ!").execute();
+            } catch (ApiException | ClientException e) {
+                log.error("Error occured", e);
+            }
+        }
+        return "ok";
+    }
+
+}
