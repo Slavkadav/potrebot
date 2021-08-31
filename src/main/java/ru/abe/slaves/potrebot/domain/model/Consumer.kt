b65c1c24100398bcfd3d6ea230f4576d277a6971
Slavnekot <slavkadav@gmail.com>
2021-08-31 21:21:19 +0300

    return missing uid for consumer entities

--- src/main/java/ru/abe/slaves/potrebot/domain/model/Consumer.kt
+++ src/main/java/ru/abe/slaves/potrebot/domain/model/Consumer.kt
@@ -1,9 +1,13 @@
 package ru.abe.slaves.potrebot.domain.model
 
+import org.springframework.data.annotation.Id
 import java.time.LocalDateTime
+import java.util.*
 
 
 data class Consumer(
+    @Id
+    var uid: UUID = UUID.randomUUID(),
     val userId: Int,
     val moneySpent: Long,
     val addTime: LocalDateTime = LocalDateTime.now()
