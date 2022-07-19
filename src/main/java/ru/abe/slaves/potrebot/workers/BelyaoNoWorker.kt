15494b2807a35878415e11ebe36def0c720d2e6e
Vyacheslav Davydov <vyacheslav.davydov@disrapp.com>
2022-07-19 11:49:26 +0300

    add belyaonet reaction

---
+++ src/main/java/ru/abe/slaves/potrebot/workers/BelyaoNoWorker.kt
@@ -0,0 +1,14 @@
+package ru.abe.slaves.potrebot.workers
+
+import org.springframework.stereotype.Component
+import ru.abe.slaves.potrebot.VkService
+import ru.abe.slaves.potrebot.web.model.VkMessage
+
+@Component
+class BelyaoNoWorker(private val vkService: VkService) : Worker {
+    override fun regex(): Regex = Regex("${regexPrefix}беляонет")
+
+    override fun reactToMessage(vkMessage: VkMessage) {
+        vkService.sendMessage(vkMessage.chatId, null, "doc-204764107_645286502")
+    }
+}
