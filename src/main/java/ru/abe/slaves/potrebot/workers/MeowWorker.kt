c65cd22b3ca005376f6dfba56ff50266767afe44
Vyacheslav Davydov <vyacheslav.davydov@disrapp.com>
2022-09-01 10:22:52 +0300

    flip the calendar

--- src/main/java/ru/abe/slaves/potrebot/workers/MeowWorker.kt
+++ src/main/java/ru/abe/slaves/potrebot/workers/MeowWorker.kt
@@ -3,6 +3,7 @@ package ru.abe.slaves.potrebot.workers
 import org.springframework.stereotype.Component
 import ru.abe.slaves.potrebot.VkService
 import ru.abe.slaves.potrebot.web.model.VkMessage
+import java.time.LocalDateTime
 
 @Component
 class MeowWorker(
@@ -15,6 +16,10 @@ class MeowWorker(
     }
 
     private fun reactToMeow(vkMessage: VkMessage) {
+        val now = LocalDateTime.now()
+        if (now.month.value == 9 && now.dayOfMonth == 3) {
+            vkService.sendMessage(vkMessage.chatId, null, "doc-204764107_648741118")
+        }
         vkService.sendMessage(vkMessage.chatId, null, "doc-204764107_644937145")
     }
 }
